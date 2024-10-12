3aee6d7d9e69a2c429fb7c55112cf339ce9d278e
dmasalex197 <dmasalex197@yandex.ru>
2024-10-12 16:17:59 +0300

    fix_review_two

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,74 +8,102 @@ export interface ICard {
 	price: number | null;
 }
 
-// Тип оплаты
-type Payment = 'онлайн' | 'при получении';
+// Способ оплаты
+type Payment = 'онлайн'| 'при получении';
 
-// Типы товара
-type CategoryCard =
-	| 'софт-скил'
-	| 'другое'
-	| 'дополнительное'
-	| 'кнопка'
-	| 'хард-скил';
-
-// Интерфейс заказа POST запрос
+// Интерфейс заказа
 export interface IOrder {
 	payment: Payment;
 	email: string;
 	phone: number;
 	adress: string;
 	total: number;
-	items: string[];
 }
 
-// Интерфейс списка заказов
-export interface ICards {
-	total: number;
-	items: ICard[];
+type EventName = string | RegExp;
+type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+// Интерфейс Api
+export interface IApi {
+	baseUrl: string;
+	get<T>(uri: string): Promise<T>;
+	post<T>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
 
-// Интерфейсы ошибок API
-export interface errorAPI {
-	error: string;
+// Интерфейс брокера событий
+export interface IEvents {
+	on<T extends object>(event: EventName, callback: (data: T) => void): void;
+	emit<T extends object>(event: string, data?: T): void;
+	trigger<T extends object>(
+		event: string,
+		context?: Partial<T>
+	): (data: T) => void;
 }
 
-// Интерфейс подтверждения заказа
-export interface ISuccessOrder {
+// Интерфейс ответа сервера заказа
+export interface IOrderResult {
 	id: string;
+}
+
+// Интерфейс получения списка карточек и отправки заказа на сервер
+export interface ICardApi {
+	getCardList: () => Promise<ICard[]>;
+	orderCards: (order: IOrder) => Promise<IOrderResult>;
+}
+
+// Интерфейс всего приложения, описывает данные cтраницы
+export interface IAppState {
+	catalog: ICard[];
+	basket: string[];
+	preview: string | null;
+	order: IOrder;
+	total: string | number;
+	loading: boolean;
+}
+
+// Интерфейс Страницы
+export interface IPage {
+	counter: number;
+	catalog: number;
+	locked: boolean;
+}
+
+// Интерфейс Карточки товара
+export interface ICardView {
+	id: string;
+	title: string;
+	category: string;
+	image: string;
+	price: number;
+	text: string;
+}
+
+// Интерфейс отображения товара в корзине
+
+export interface ICardBasketView {
+	title: string;
+	price: number;
+	index: number;
+}
+
+// Интерфейс отображения корзины
+export interface IBasketView {
+	list: HTMLElement[];
 	total: number;
 }
 
-// Интерфейс API клиента
-export interface ICardAPI {
-	getCards: () => Promise<ICards>;
-	getCard: (id: string) => Promise<ICard>;
-	orderCards: (order: IOrder) => Promise<ISuccessOrder>;
+// Интерфейс модального окна
+export interface IModalData {
+	content: HTMLElement;
 }
 
-// Типы запросов Api
-export type ApiMethods = 'GET' | 'POST' | 'PUT' | 'DELETE';
-
-// Интерфейс корзины
-export interface IBasket {
-	basketTotal: number;
-	title: string;
-	items: ICard[];
+// Интерфейс класса Form
+interface IForm {
+	errors: string[];
+	valid: boolean;
 }
 
-// Интерфейс для модели данных товаров
-export interface ICardModel {
-    cards: ICard[];
-    preview: string | null;
-    loadCards(): Promise<void>;
-    getCardById(cardId: string): Promise<ICard>;
-}
-
-// Интерфейс для модели данных корзины
-export interface IBasketModel {
-    total: number;
-    cards: Map<string, number>;
-    add(id: string): void;
-    remove(id: string): void;
-    clear(): void;
+// Интерфейс ответа успешного заказа
+interface ISuccess {
+	total: number;
 }
